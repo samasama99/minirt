2ca46faeff6433ece13bb328d4f3c27a9a97d2cf
zsarir23 <zainebsarir@gmail.com>
2022-07-22 23:19:20 +0100

    norm src/prof.c

--- src/prof.c
+++ src/prof.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-typedef struct timeval t_time;
+typedef struct timeval	t_time;
 
-long time_now()
+long	time_now(void)
 {
-  t_time time;
-  gettimeofday(&time, NULL);
-  return time.tv_sec * 1000 + time.tv_usec / 1000;
+	t_time	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
