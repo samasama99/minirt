bf961f9a781a1a5468df16fcd4319eab04b03eaa
zsarir23 <zainebsarir@gmail.com>
2022-07-23 19:20:22 +0100

    src/render.c

--- src/ray.c
+++ src/ray.c
@@ -13,7 +13,6 @@ t_point	ray_position(t_ray r, double t)
 	return (sum(r.origin, scalar(r.direction, t)));
 }
 
-
 t_ray	ray_transform(t_ray ray, t_matrix4 m)
 {
 	return ((t_ray){
@@ -22,29 +21,27 @@ t_ray	ray_transform(t_ray ray, t_matrix4 m)
 	});
 }
 
-t_vec normal_at(t_shape shape, t_point world_point)
+t_vec	normal_at(t_shape shape, t_point world_point)
 {
+	t_vec				local_normal;
+	t_vec				world_normal;
 	const t_transform	shape_transform = shape.super.transform;
 	const t_point		local_point = apply(inverse(shape_transform),
-																world_point);
-	t_vec	local_normal;
-	t_vec	world_normal;
+								world_point);
 
 	if (shape.type == SuperShape)
-		local_normal = vector(local_point.x,
-									local_point.y,
-									local_point.z);
+		local_normal = vector(local_point.x, local_point.y, local_point.z);
 	if (shape.type == Plane)
 		local_normal = normal_at_plane(shape.plane);
 	if (shape.type == Sphere)
-		local_normal =  normal_at_sphere(shape.sphere, local_point);
+		local_normal = normal_at_sphere(shape.sphere, local_point);
 	world_normal = apply(transpose(inverse(shape_transform)),
-													local_normal);
+			local_normal);
 	world_normal.w = 0;
 	return (normalize(world_normal));
 }
 
-t_material	material()
+t_material	material(void)
 {
 	return ((t_material){
 		.color = color(1, 1, 1),
@@ -63,7 +60,7 @@ bool	is_inside_shape(t_vec normalv, t_vec eyev)
 
 t_comp	prepare_computations(t_intersection i, t_ray r)
 {
-	t_comp comp;
+	t_comp	comp;
 
 	comp.t = i.t;
 	comp.shape = i.shape;
@@ -74,27 +71,9 @@ t_comp	prepare_computations(t_intersection i, t_ray r)
 	{
 		comp.inside = true;
 		comp.normalv = opose(comp.normalv);
-	} 
+	}
 	else
 		comp.inside = false;
 	comp.over_point = sum(comp.point, scalar(comp.normalv, EPSILON));
-	return comp;
+	return (comp);
 }
-
-
-// e is the origin of the ray and d is the direction
-// p(t) = e + td
-// (p - c).(p - c) - R^2 = 0
-// (d.d)t^2 + 2d.(e - c)t + (e - c).(e - c) - R^2 = 0
-// discriminant  = B^2 - 4AC if there is a solution then discriminant >= 0
-// bool is_hit(const t_sphere sp, const t_ray r) {
-//     const t_ray ray = ray_transform(r, inverse(sp.t));
-//     t_vec e_c = sub(ray.origin, sp.center); // e - c
-//     double a = dot(ray.direction, ray.direction);
-//     double b = 2.0 * dot(e_c, ray.direction);
-//     double c = dot(e_c, e_c) - sp.radius * sp.radius;
-//     double discriminant = b * b - 4 * a * c;
-//     if (discriminant  < 0)
-//         return false;
-//     return true;
-// 
