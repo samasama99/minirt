b0675247536a273fc109d0d60209fe4f089c6804
Oussama Rahmouni <orahmoun@e2r6p12.1337.ma>
2022-08-19 15:33:29 +0100

    cheking .rt extension

--- main.c
+++ main.c
@@ -12,19 +12,40 @@
 
 #include "src/parsing.h"
 
+bool isRtFile(char *file) {
+  char *dot;
+
+  if (file == NULL)
+    return false;
+  dot = strrchr(file, '.');
+  if (dot == NULL || file == dot)
+    return false;
+  return is_equal_str(dot, ".rt");
+}
+
 void	ft_perror(int exit_status)
 {
 	perror("Error");
 	exit(exit_status);
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	const t_res	canvas_size = pair(1200, 675);
 	const t_res	res = pair(canvas_size.x, canvas_size.y);
-	const int	fd = open("src/test/test2.rt", O_RDONLY);
+	int	fd;
 	t_data		data;
-
+  
+  if (ac != 2) {
+      ft_putendl_fd("minirt only accepet one argument {*.rt}", 2);
+      return 1;
+  }
+  if (!isRtFile(av[1]))
+  {
+      ft_putendl_fd("pls provide a .rt file", 2);
+      return 2;
+  }
+  fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 		ft_perror(1);
 	data = (t_data){};
